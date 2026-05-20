12b1a7ff368c9a86a126db405b132d3d47166b4d
ankit kushwaha <kushwahaankit02042@gmail.com>
2026-05-20 08:04:40 +0530

    Implement Bluetooth control for appliance 1

--- Task2_Bluetooth_Automation.ino
+++ Task2_Bluetooth_Automation.ino
@@ -1,1 +1,24 @@
-
+const int appliance1 = 12; 
+const int appliance2 = 13; 
+char bluetoothData;              
+void setup() {
+  Serial.begin(9600); 
+  pinMode(appliance1, OUTPUT);
+  pinMode(appliance2, OUTPUT);
+  Serial.println("Bluetooth Automation Ready!");
+  Serial.println("Send 1 for ON, 0 for OFF");
+}
+void loop() {
+  if (Serial.available() > 0) {
+    bluetoothData = Serial.read(); 
+    
+    if (bluetoothData == '1') {
+      digitalWrite(appliance1, HIGH);
+      Serial.println("Appliance 1 ON");
+    } 
+    else if (bluetoothData == '0') {
+      digitalWrite(appliance1, LOW);
+      Serial.println("Appliance 1 OFF");
+    } 
+  }
+}
